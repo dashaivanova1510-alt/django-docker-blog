ae95fb7481ddebdbf680c42e44ae67f3605ee9c7
Dasha <dashaivanova1510@gmail.com>
2025-11-11 20:12:44 +0200

    feat(models): Update Article and Comment models to use User ForeignKey

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     title = models.CharField(max_length=255, verbose_name="Назва")
@@ -24,7 +25,7 @@ class Tag(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
-    author = models.CharField(max_length=255, verbose_name="Автор")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
     text = models.TextField(verbose_name="Текст статті")
     image = models.URLField(verbose_name="Посилання на зображення")
     publication_date = models.DateField(auto_now_add=True, verbose_name="Дата публікації")
@@ -42,9 +43,16 @@ class Article(models.Model):
 
 class Comment(models.Model):
     text = models.TextField(verbose_name="Текст коментаря")
-    author = models.CharField(max_length=255, verbose_name="Автор")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Автор")
+    author_name = models.CharField(max_length=255, null=True, blank=True, verbose_name="Ім'я анонімного автора")
     publication_date = models.DateField(auto_now_add=True, verbose_name="Дата публікації")
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments', verbose_name="Стаття")
+
+    def get_author_name(self):
+        """Повертає ім'я автора: або з профілю, або з поля author_name."""
+        if self.author:
+            return self.author.username
+        return self.author_name or "Анонім"
 
     def __str__(self):
         return f"Коментар від {self.author} до статті '{self.article.title}'"
