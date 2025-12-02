5ef5f61e0ad3369d13ce82124e740d6937ed2760
Dasha <dashaivanova1510@gmail.com>
2025-12-02 21:05:32 +0200

    Vue page for article list

---
+++ frontend/src/stores/articles.js
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useArticleStore = defineStore('articleStore', {
+  state: () => ({
+    articles: [],
+    loading: false
+  }),
+  actions: {
+    async fetchArticles() {
+      this.loading = true
+      try {
+        const response = await axios.get('http://localhost:8000/api/articles/')
+        this.articles = response.data
+      } catch (error) {
+        console.error(error)
+      } finally {
+        this.loading = false
+      }
+    },
+    async fetchArticleById(id) {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/articles/${id}/`)
+        return response.data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+})
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useArticleStore } from '../stores/articles'
+import { onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const store = useArticleStore()
+
+onMounted(() => {
+  store.fetchArticles()
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Articles List</h1>
+    <div v-if="store.loading">Loading...</div>
+    <div v-else>
+      <div v-for="article in store.articles" :key="article.id">
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.content }}</p>
+        <RouterLink :to="'/articles/' + article.id">Read more</RouterLink>
+        <hr>
+      </div>
+    </div>
   </main>
 </template>
